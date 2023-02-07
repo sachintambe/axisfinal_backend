581f5eca6f31c3d9187fb0ef057e415f9f4d997d
sachintambe <sachintambe867@gmail.com>
2023-02-07 12:02:34 +0530

    axos saral backend

--- src/test/java/com/axis/saral/AxisSaralApplicationTests.java
+++ src/test/java/com/axis/saral/AxisSaralApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AxisSaralApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
